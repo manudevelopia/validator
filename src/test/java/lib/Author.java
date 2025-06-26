087e45e7aeea7cf3f35aaf7a6403384017040b52
manudevelopia <manugarciarodriguez@gmail.com>
2025-06-26 18:53:54 +0200

    use object

--- src/test/java/lib/Author.java
+++ src/test/java/lib/Author.java
@@ -3,8 +3,14 @@ package lib;
 import info.developia.lib.annotation.Length;
 import info.developia.lib.annotation.NotNull;
 
-public record Author(
-        @NotNull String name,
-        @Length(min = 3, max = 10) String surname
-) {
+public class Author {
+    @NotNull
+    private final String name;
+    @Length(min = 3, max = 10)
+    private final String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 }
--- src/test/java/lib/Book.java
+++ src/test/java/lib/Book.java
@@ -3,9 +3,16 @@ package lib;
 import info.developia.lib.annotation.NotNull;
 import info.developia.lib.annotation.Number;
 
-public record Book(
-        @NotNull String title,
-        Author author,
-        @Number(min = 1, max = 1000, positive = true) int pages
-) {
+public class Book {
+    @NotNull
+    private final String title;
+    private final Author author;
+    @Number(min = 1, max = 100, positive = true)
+    private final int pages;
+
+    public Book(String title, Author author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
 }
